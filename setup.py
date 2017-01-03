6f5831dc9b9f92d36eac89493bfd752fb8543de9
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-01-03 10:32:54 +0100

    use readme.rst instead of readme.md as pypi doc

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import codecs
 
 here = path.abspath(path.dirname(__file__))
 
-with codecs.open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with codecs.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
