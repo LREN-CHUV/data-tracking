63b1fb1879bcd6ecd732872d18d5e5c3521f0de9
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-01-12 12:23:03 +0100

    adapt test to new specs

--- tests/test.py
+++ tests/test.py
@@ -19,6 +19,7 @@ class BasicTestSuite(unittest.TestCase):
         self.scan_date = datetime.datetime(2014, 7, 23, 0, 0)
         self.dcm_files_pattern = "**/MR.*"
         self.nii_files_pattern = "**/*.nii"
+        self.nifti_type = "nifti"
 
     def test_dicom_extract(self):
         assert dicom_import.visit_info(self.dcm_folder, self.dcm_files_pattern, self.db_url)[0] in [self.pid]
@@ -28,8 +29,8 @@ class BasicTestSuite(unittest.TestCase):
 
     def test_nifti_extract(self):
         nifti_import.nifti2db(self.nii_folder, self.pid, self.scan_date, self.nii_files_pattern, self.db_url)
-        print(nifti_import.conn.db_session.query(nifti_import.conn.Nifti).count())
-        assert nifti_import.conn.db_session.query(nifti_import.conn.Nifti).count() == 2
+        assert nifti_import.conn.db_session.query(nifti_import.conn.DataFile).filter_by(
+            type=self.nifti_type).count() == 2
 
 if __name__ == '__main__':
     unittest.main()
