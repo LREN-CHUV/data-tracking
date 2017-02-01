14b500680c6176c1c023197ef3ba6055dff9bdfd
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-02-01 11:06:15 +0100

    fix test return value

--- tests/test.sh
+++ tests/test.sh
@@ -13,6 +13,7 @@ fi
 # Run unit tests
 echo "Running unit tests..."
 nosetests unittest.py
+ret=$?
 
 # Remove DB container (if not on CircleCI)
 if [ -z "$CIRCLECI" ] || [ "$CIRCLECI" = false ] ; then
@@ -20,3 +21,5 @@ if [ -z "$CIRCLECI" ] || [ "$CIRCLECI" = false ] ; then
     docker kill ${db_docker_id}
     docker rm -f ${db_docker_id}
 fi
+
+exit "$ret"
