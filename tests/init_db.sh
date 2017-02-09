bcdb83d3ba81c68775841be426331e4074e1bee2
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-02-09 18:03:19 +0100

    replace db submodule by Docker image for testing

--- tests/init_db.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd db
-cp alembic.ini.sample alembic.ini
-alembic upgrade head
-rm alembic.ini
-cd ..
--- tests/test.sh
+++ tests/test.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
-# Start DB container (if not on CircleCI)
+# Start DB container
 echo "Starting DB container..."
-if [ -z "$CIRCLECI" ] || [ "$CIRCLECI" = false ] ; then
-    db_docker_id=$(docker run -d -p 5432:5432 -e 'POSTGRES_PASSWORD=postgres' postgres)
+db_docker_id=$(docker run -d -p 5432:5432 hbpmip/data-catalog-db)
 
-    # Wait for DB to be ready
-    echo "Waiting for DB to be ready..."
-    sleep 5  # TODO: replace this by a test
-fi
-
-# Create the DB tables
-echo "Creating DB tables..."
-./init_db.sh
+# Wait for DB to be ready
+echo "Waiting for DB to be ready..."
+sleep 5  # TODO: replace this by a test
 
 # Run unit tests
 echo "Running unit tests..."
