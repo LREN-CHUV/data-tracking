c4d58b9500ade645cc90cb91c44f946ff55c6c96
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-03-06 15:38:09 +0100

    add automatic notifications on slack when publishing new release on PyPi

--- publish.sh
+++ publish.sh
@@ -5,3 +5,10 @@
 
 # Push on PyPi
 twine upload dist/*
+
+# Notify on slack
+sed "s/USER/${USER^}/" $WORKSPACE/slack.json > $WORKSPACE/.slack.json
+sed -i.bak "s/VERSION/$(git describe)/" $WORKSPACE/.slack.json
+curl -k -X POST --data-urlencode payload@$WORKSPACE/.slack.json https://hbps1.chuv.ch/slack/dev-activity
+rm -f $WORKSPACE/.slack.json
+rm -f $WORKSPACE/.slack.json.bak
