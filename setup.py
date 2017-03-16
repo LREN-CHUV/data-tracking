5ab6fac5682e3253c22db5243aaa65658d7ce626
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-03-16 12:57:22 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ with codecs.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='mri-meta-extract',
     version='1.4.6',
+    zip_safe=False,
     url='https://github.com/LREN-CHUV/mri-meta-extract',
     description='Extract meta-data from DICOM and NIFTI files',
     long_description=long_description,
@@ -17,8 +18,23 @@ setup(
     author_email='mirco.nasuti@chuv.ch',
     license='Apache 2.0',
     packages=['mri_meta_extract'],
-    extras_require={
-        'test': ['unittest', 'nose'],
-    },
-    install_requires=['airflow', 'pydicom', 'sqlalchemy', 'python-magic', 'nibabel', 'psycopg2']
+    keywords='mri dicom nifti',
+    install_requires=[
+        'airflow>=1.7.0',
+        'pydicom>=0.9.9',
+        'SQLAlchemy>=1.1.6',
+        'python-magic>=0.4.12',
+        'nibabel>=2.1.0',
+        'psycopg2>=2.7.1'],
+    classifiers=(
+        'Environment :: Library',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Operating System :: Unix',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    )
 )
