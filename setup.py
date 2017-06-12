3e6d7cea95ef2d09f8c833d49a89a3fed70f2fd1
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-06-12 09:36:58 +0200

    Bump version: 1.5.12 → 1.5.13

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with codecs.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='data-tracking',
-    version='1.5.12',
+    version='1.5.13',
     zip_safe=False,
     url='https://github.com/LREN-CHUV/data-tracking',
     description='Extract meta-data from DICOM and NIFTI files',
