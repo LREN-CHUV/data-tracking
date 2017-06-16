844561c2dd2a70a06877e7df48fc6841567d9c4a
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-06-16 11:26:01 +0200

    bugfix

--- data_tracking/connection.py
+++ data_tracking/connection.py
@@ -47,14 +47,14 @@ class Connection:
     def get_participant_id(self, participant_name, dataset):
         participant_name = str(participant_name)
         participant = self.db_session.query(self.ParticipantMapping).filter_by(
-            dataset=dataset, name=participant_name).one_or_none().id
+            dataset=dataset, name=participant_name).one_or_none()
         if not participant:
             participant = self.ParticipantMapping(dataset=dataset, name=participant_name,
                                                   participant_id=self.new_participant_id())
             self.db_session.merge(participant)
             self.db_session.commit()
         return self.db_session.query(self.ParticipantMapping).filter_by(
-            dataset=dataset, name=participant_name).one_or_none().id
+            dataset=dataset, name=participant_name).one_or_none().participant_id
 
     def new_visit_id(self):
         try:
@@ -66,18 +66,18 @@ class Connection:
     def get_visit_id(self, visit_name, dataset):
         visit_name = str(visit_name)
         visit = self.db_session.query(self.VisitMapping).filter_by(
-            dataset=dataset, name=visit_name).one_or_none().id
+            dataset=dataset, name=visit_name).one_or_none()
         if not visit:
             visit = self.VisitMapping(dataset=dataset, name=visit_name, visit_id=self.new_visit_id())
             self.db_session.merge(visit)
             self.db_session.commit()
         return self.db_session.query(self.VisitMapping).filter_by(
-            dataset=dataset, name=visit_name).one_or_none().id
+            dataset=dataset, name=visit_name).one_or_none().visit_id
 
     def get_session_id(self, session_name, visit_id):
         session_name = str(session_name)
         session = self.db_session.query(self.Session).filter_by(
-            name=session_name, visit_id=visit_id).one_or_none().id
+            name=session_name, visit_id=visit_id).one_or_none()
         if not session:
             session = self.Session(name=session_name, visit_id=visit_id)
             self.db_session.merge(session)
@@ -88,7 +88,7 @@ class Connection:
     def get_sequence_id(self, sequence_name, session_id):
         sequence_name = str(sequence_name)
         sequence = self.db_session.query(self.Sequence).filter_by(
-            name=sequence_name, session_id=session_id).one_or_none().id
+            name=sequence_name, session_id=session_id).one_or_none()
         if not sequence:
             sequence = self.Sequence(name=sequence_name, session_id=session_id)
             self.db_session.merge(sequence)
@@ -99,7 +99,7 @@ class Connection:
     def get_repetition_id(self, repetition_name, sequence_id):
         repetition_name = str(repetition_name)
         repetition = self.db_session.query(self.Repetition).filter_by(
-            name=repetition_name, sequence_id=sequence_id).one_or_none().id
+            name=repetition_name, sequence_id=sequence_id).one_or_none()
         if not repetition:
             repetition = self.Repetition(name=repetition_name, sequence_id=sequence_id)
             self.db_session.merge(repetition)
